3c2a165b3f0f4fd1c41ba6da93f4eb3056fa6f65
Victor Lam <victorebox@yahoo.com.hk>
2013-05-02 12:56:37 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveAdminAuth::Application.config.secret_token = '695d14080186ca52d4f00a061d9c42d642ef2c581fcbe3d0816d67b9919e8d450ec61c70c162247895b0cca4768f3e9850895917bf0e7f89692803eeaad1ae2e'
