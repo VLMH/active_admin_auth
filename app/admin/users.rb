2808fdf5d683cb3917adf1824f6a43326b9a2f7a
Victor Lam <victorebox@yahoo.com.hk>
2013-05-03 12:48:11 +0800

    make active admin user resource work with strong_parameters

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register User do
-  
+
+  # make it work with strong parameters
+  controller do
+    def resource_params
+      return [] if request.get?
+      user_params = params[:user]
+      user_params[:password] = user_params[:password_confirmation] = user_params[:encrypted_password]
+      [ params.require(:user).permit(:email, :password, :password_confirmation) ]
+    end
+  end
+
 end
