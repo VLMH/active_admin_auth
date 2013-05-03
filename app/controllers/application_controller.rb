b158b62a33c1bbdc1c6f0feb0bb24adbbefc6635
Victor Lam <victorebox@yahoo.com.hk>
2013-05-03 12:47:35 +0800

    fix wrong method name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_admin_user!
     authenticate_user!
-    unless current_user.has_role "admin"
+    unless current_user.has_role? :admin
       raise ActionController::RoutingError.new('Route Not Found')
     end
   end
