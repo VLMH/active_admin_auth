dfaf4024327b7f45f2579bb011e2e30bb96a3683
Victor Lam <victorebox@yahoo.com.hk>
2013-05-03 11:13:51 +0800

    change active admin setting re-implement authenticate_active_admin_user!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def authenticate_admin_user!
-    raise CanCan::AccessDenied.new("Not authorized!")
-
-    # return nil if user_signed_in? && current_user.has_role?("admin")
-    # current_user
-  end
-
-  def current_admin_user
-    current_user if user_signed_in? && current_user.has_role?("admin")
+    authenticate_user!
+    unless current_user.has_role "admin"
+      raise ActionController::RoutingError.new('Route Not Found')
+    end
   end
 end
